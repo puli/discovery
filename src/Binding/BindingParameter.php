651b8166aa8778e9c9fffbfe51b0f90ef8449787
Bernhard Schussek <bschussek@gmail.com>
2014-12-16 15:19:48 +0100

    Added BindingParameter

---
+++ src/Binding/BindingParameter.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of the puli/discovery package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Puli\Discovery\Binding;
+
+/**
+ * A parameter that can be set during binding.
+ *
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class BindingParameter
+{
+    /**
+     * Mode: the parameter must be set
+     */
+    const REQUIRED = 1;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var int
+     */
+    private $mode;
+
+    /**
+     * Creates a new parameter.
+     *
+     * @param string   $name The parameter name.
+     * @param int|null $mode A bitwise combination of the parameter's mode
+     *                       constants.
+     */
+    public function __construct($name, $mode = null)
+    {
+        $this->name = $name;
+        $this->mode = (int) $mode;
+    }
+
+    /**
+     * Returns the name of the parameter.
+     *
+     * @return string The parameter name.
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Returns the parameter mode.
+     *
+     * @return int A bitwise combination of the parameter's mode constants.
+     */
+    public function getMode()
+    {
+        return $this->mode;
+    }
+
+    /**
+     * Returns whether the parameter must be set.
+     *
+     * This method is the inverse of {@link isOptional()}.
+     *
+     * @return bool Returns `true` if the parameter must be set.
+     */
+    public function isRequired()
+    {
+        return (bool) ($this->mode & self::REQUIRED);
+    }
+
+    /**
+     * Returns whether the parameter is optional.
+     *
+     * This method is the inverse of {@link isRequired()}.
+     *
+     * @return bool Returns `true` if the parameter is optional.
+     */
+    public function isOptional()
+    {
+        return !$this->isRequired();
+    }
+}
---
+++ tests/Binding/BindingParameterTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the puli/discovery package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Puli\Discover\Tests\Binding;
+
+use Puli\Discovery\Binding\BindingParameter;
+
+/**
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class BindingParameterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetName()
+    {
+        $param = new BindingParameter('name');
+
+        $this->assertSame('name', $param->getName());
+    }
+
+    public function testIsOptionalByDefault()
+    {
+        $param = new BindingParameter('name');
+
+        $this->assertTrue($param->isOptional());
+        $this->assertFalse($param->isRequired());
+    }
+
+    public function testIsNotOptionalIfRequired()
+    {
+        $param = new BindingParameter('name', BindingParameter::REQUIRED);
+
+        $this->assertFalse($param->isOptional());
+        $this->assertTrue($param->isRequired());
+    }
+}
